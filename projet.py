2f4a5adb6d44e60878761767c47cc0d440c6d59f
ghayth2 <raynerbruan5@gmail.com>
2025-03-11 12:35:50 +0100

    seul commit apres merge

--- projet.py
+++ projet.py
@@ -1,11 +1,15 @@
-
 def somme(T):
     s=0
     for t in T:
        s+=t
  	return s
-
 Data=[1,3,5]
 Som=sum(Data)
 print('la somme est :', som)
+if Data:
+	print('la somme est :', sum(Data))
+	print(' le min est : ', min(Data))
+	print('le max : ', max(Data))
+else :
+	print('dossier vide')
 
